8283c6cc55c1ac42f519fe5fe944b03a591a01e2
wifimoon <vyache-nov@mail.ru>
2022-08-22 13:57:02 +0300

    returned main

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app.web.app import setup_app
 from aiohttp.web import run_app
 
 if __name__ == "__main__":
-    run_app(setup_app(config_path=os.path.join(os.path.dirname(__file__), 'config.yml')), host='127.0.0.1')
+    run_app(setup_app(config_path=os.path.join(os.path.dirname(__file__), 'config.yml')))
